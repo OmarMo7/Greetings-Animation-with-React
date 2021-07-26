801066a9248ed4a70863805d932678a9d82506e6
OmarMo7 <omar7.first@gmail.com>
2021-07-26 09:27:00 +0200

    Setting up consts and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import Transition from './components/Transition';
 
 function App() {
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  const Trans = [
+    "te-flip1",
+    "te-flip2",
+    "te-flip3",
+    "te-flip4",
+    "te-rotation1",
+    "te-rotation2",
+    "te-rotation3",
+    "te-rotation4",
+    "te-rotation5",
+    "te-multiflip1",
+    "te-multiflip2",
+    "te-multiflip3",
+    "te-cube1",
+    "te-cube2",
+    "te-cube3",
+    "te-cube4",
+    "te-unfold1",
+    "te-unfold2",
+    "te-example1",
+    "te-example2",
+    "te-example3",
+    "te-example4",
+    "te-example5",
+    "te-example6",
+    "te-example7"
+  ]
+
   return (
     <div className="App">
-      <Transition />
+      <Transition random={randomNumber} Trans={Trans} />
     </div>
   );
 }
