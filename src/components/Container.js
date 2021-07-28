5b7521724d4bcea6f7bf8a1b3389513c0b451034
OmarMo7 <omar7.first@gmail.com>
2021-07-28 18:53:49 +0200

    Moving the content of Container to Wrapper

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,48 +1,12 @@
 import React, { Component } from 'react'
-import Transition from '../components/Transition';
-import Cover from '../components/Cover';
+import Wrapper from './Wrapper'
 
 
 class Container extends Component {
-
-  state = {
-    randNum: 0,
-    last: 0,
-    current: 0
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        randNum: this.randomNumber(0, 17)
-      })
-      this.updateImages()
-    }, 4000)
-  }
-
-  randomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-  updateImages = () => {
-    const ImgsLength = this.props.Imgs.length
-    this.state.current === ImgsLength - 1 ?
-      this.setState({ current: 0, last: ImgsLength - 1 }) :
-      this.setState({ last: this.state.current, current: this.state.current + 1 })
-  }
-
   render() {
-
-    const Trans = this.props.Trans
-    const Imgs = this.props.Imgs
-
-    const ImgsLength = Imgs.length
-
     return (
       <div className="te-container">
-        <Cover Imgs={Imgs} current={this.state.current} />
-        <Transition lenth={ImgsLength} transName={Trans[this.state.randNum].slice(0, -1)} randNum={this.state.randNum}
-          transType={Trans[this.state.randNum].slice(-1, Trans.length)} Trans={Trans} Imgs={Imgs} last={this.state.last} current={this.state.current} />
+        <Wrapper Trans={this.props.Trans} Imgs={this.props.Imgs} />
       </div>
     )
   }
