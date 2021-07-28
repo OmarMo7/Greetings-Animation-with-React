e1af989c2f25fb8f2afe6e3eaed3c95ece754c8a
OmarMo7 <omar7.first@gmail.com>
2021-07-28 18:52:06 +0200

    DerivingState + del constructor + fix bug of names

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -2,46 +2,36 @@ import React, { Component } from 'react'
 
 
 class Transition extends Component {
-  constructor(props) {
-    super(props)
-    this.state.Trans = this.props.Trans
-    this.state.Imgs = this.props.Imgs
-  }
 
   state = {
     randNum: 0,
     transName: "",
     transType: "",
     last: 0,
-    current: 0,
-    Imgs: [],
-    Trans: []
+    current: 0
   }
+
+  static getDerivedStateFromProps(props, state) {
+    return {
+      randNum: props.randNum,
+      transName: props.transName,
+      transType: props.transType,
+      current: props.current,
+      last: props.last
+    }
+  }
+
 
   componentDidMount() {
-    setInterval(() => {
-      let num = 0
-      num = this.props.random(0, 17)
-      this.setState({
-        randNum: num,
-        transName: this.props.Trans[num].slice(0, -1), transType:
-          this.props.Trans[num].slice(-1, this.props.Trans.length),
-      })
-      this.updateImages()
-    }, 4000)
+
   }
 
-  updateImages = () => {
-    this.state.current === this.state.Imgs.length - 1 ?
-      this.setState({ current: 0, last: this.state.Imgs.length - 1 }) :
-      this.setState({ last: this.state.current, current: this.state.current + 1 })
-  }
 
   render() {
     switch (this.state.transName) {
       case 'te-flip':
         return (
-          <div className={"te-transition te-flip " + this.state.transType}>
+          <div className={"te-transition te-flip" + this.state.transType}>
             <div className="te-card">
               <div className="te-front">
                 <img alt="" src={this.props.Imgs[this.state.last]}></img>
@@ -55,7 +45,7 @@ class Transition extends Component {
         )
       case 'te-rotation':
         return (
-          <div className={"te-transition te-rotation " + this.state.transType}>
+          <div className={"te-transition te-rotation" + this.state.transType}>
             <div className="te-front">
               <img alt="" src={this.props.Imgs[this.state.last]}></img>
             </div>
@@ -66,7 +56,7 @@ class Transition extends Component {
         )
       case 'te-multiflip':
         return (
-          <div className={"te-transition te-multiflip " + this.state.transType}>
+          <div className={"te-transition te-multiflip" + this.state.transType}>
             <div className="te-card te-flip1">
               <div className="te-front">
                 <img alt="" src={this.props.Imgs[this.state.last]}></img>
@@ -127,7 +117,7 @@ class Transition extends Component {
         )
       case 'te-cube':
         return (
-          <div className={"te-transition te-cube " + this.state.transType}>
+          <div className={"te-transition te-cube" + this.state.transType}>
             <div className="te-cube-front te-cube-face te-front"><img alt="" src={this.props.Imgs[this.state.last]}></img></div>
             <div className="te-cube-top te-cube-face te-back"><img alt="" src={this.props.Imgs[this.state.current]}></img>
             </div>
@@ -141,7 +131,7 @@ class Transition extends Component {
         )
       case 'te-unfold':
         return (
-          <div className={"te-transition te-unfold " + this.state.transType}>
+          <div className={"te-transition te-unfold" + this.state.transType}>
             <div className="te-front te-front1"><img alt="" src={this.props.Imgs[this.state.last]}></img>
             </div>
             <div className="te-front te-front2">
