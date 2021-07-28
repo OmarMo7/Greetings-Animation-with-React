79a19f3fb1941518d56e874bea5f70951c99fff1
OmarMo7 <omar7.first@gmail.com>
2021-07-28 18:54:08 +0200

    Shadow created

---
+++ src/components/Shadow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Shadow extends Component {
+  render() {
+    return (
+      <div className="te-shadow">
+        
+      </div>
+    )
+  }
+}
+
+export default Shadow
