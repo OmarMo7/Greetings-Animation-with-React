7b7e48effa17cb91d40ce524b7450d377ee31a7f
OmarMo7 <omar7.first@gmail.com>
2021-08-02 19:36:13 +0200

    Removing te-hide class

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 class Cover extends Component {
   render() {
     return (
-      <div className="te-cover te-hide">
+      <div className="te-cover">
         <img alt="" src={this.props.Imgs[this.props.current]} />
       </div>
     )
