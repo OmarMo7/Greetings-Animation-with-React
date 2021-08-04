a35bfa431f2484861726fa39da25745e8bd13283
OmarMo7 <omar7.first@gmail.com>
2021-08-04 10:11:04 +0200

    Moving some files

---
+++ src/Cover.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class Cover extends Component {
+  render() {
+    return (
+      <div className="te-cover" >
+        <img style={{ width: this.props.Imgs[this.props.current].width, height: this.props.Imgs[this.props.current].height }} alt="" src={this.props.Imgs[this.props.current].img} />
+      </div>
+    )
+  }
+}
+
+export default Cover
