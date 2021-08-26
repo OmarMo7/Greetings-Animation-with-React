9d9a1bd841bf944688d53230389bbaaa88621783
OmarMo7 <omar7.first@gmail.com>
2021-08-26 18:27:36 +0200

    Moving Trans and Imgs to Container instead

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,11 +3,57 @@ import Wrapper from './Wrapper'
 
 
 class Container extends Component {
+  constructor(props) {
+    super(props);
+    var Trans = [
+      "te-flip1",
+      "te-flip2",
+      "te-flip3",
+      "te-flip4",
+      "te-rotation1",
+      "te-rotation2",
+      "te-rotation3",
+      "te-rotation4",
+      "te-rotation5",
+      "te-multiflip1",
+      "te-multiflip2",
+      "te-multiflip3",
+      "te-cube1",
+      "te-cube2",
+      "te-cube3",
+      "te-cube4",
+      "te-unfold1",
+      "te-unfold2",
+      "te-example1",
+      "te-example2",
+      "te-example3",
+      "te-example4",
+      "te-example5",
+      "te-example6",
+      "te-example7"
+    ]
+    var All_Imgs = document.querySelectorAll('.img-transition')
+    var Imgs = []
+    All_Imgs.forEach(Img => {
+      var content = {
+        img: Img.src,
+        width: 329,
+        height: 425
+      };
+      Imgs.push(content)
+    })
+
+    this.state = {
+      Trans,
+      Imgs
+    }
+  }
+
 
   render() {
     return (
       <div className="te-container">
-        <Wrapper Trans={this.props.Trans} Imgs={this.props.Imgs} />
+        <Wrapper Trans={this.state.Trans} Imgs={this.state.Imgs} />
       </div>
     )
   }
