0b370fcc75f333b99f88fbf750ddf2c95ec068f5
OmarMo7 <omar7.first@gmail.com>
2021-08-26 18:24:32 +0200

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
 import Form from './components/Form';
+import Container from './components/Container';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 //TODO: Justa get the string value of src instead of processing the whole img tag in index.html
 
 function App() {
-  const Trans = [
-    "te-flip1",
-    "te-flip2",
-    "te-flip3",
-    "te-flip4",
-    "te-rotation1",
-    "te-rotation2",
-    "te-rotation3",
-    "te-rotation4",
-    "te-rotation5",
-    "te-multiflip1",
-    "te-multiflip2",
-    "te-multiflip3",
-    "te-cube1",
-    "te-cube2",
-    "te-cube3",
-    "te-cube4",
-    "te-unfold1",
-    "te-unfold2",
-    "te-example1",
-    "te-example2",
-    "te-example3",
-    "te-example4",
-    "te-example5",
-    "te-example6",
-    "te-example7"
-  ]
-
-  var All_Imgs = document.querySelectorAll('.img-transition')
-  var Imgs = []
-  All_Imgs.forEach(Img => {
-    var content = {
-      img: Img.src,
-      width: 329,
-      height: 425
-    };
-    Imgs.push(content)
-  })
-
 
   return (
     <div className="App">
       {/* <Container Trans={Trans} Imgs={Imgs} /> */}
-      <Form Trans={Trans} Imgs={Imgs} />
+
+      <BrowserRouter>
+        {/* <Form Trans={Trans} Imgs={Imgs} /> */}
+        <Route exact path='/' component={Form} />
+        <Route path='/user' component={Container} />
+      </BrowserRouter>
     </div>
   );
 }
